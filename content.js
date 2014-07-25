a2db1e5e504bd0495b093ab0fab7a96197b610c4
Tim Whidden <twhid@twhid.com>
2014-07-25 18:30:49 -0400

    initial commit

--- content.js
+++ content.js
@@ -1,22 +1,35 @@
+var request = {method: "getLocalStorage", key: "jira"};
 
-chrome.extension.sendRequest({method: "getLocalStorage", key: "jira"}, function(response) {
-	var pattern = /([A-Z]{2,}-\d+)/g;
-	var jira = response.data + '/browse/';
+chrome.extension.sendRequest(request, function(response) {
+    //
+    // selectors
+    // ---------------------------------------------------------------------
+    var PR_TITLE = '.js-issue-title';
+    var COMMIT_MESSAGE = '.commit-message code .message';
+    // ---------------------------------------------------------------------
+    //
 
-    // selectors in which to look for jira issues
-	var selectors = $('h2.content-title, p.commit-title');
+    var title = $(PR_TITLE);
+    var commits = $(COMMIT_MESSAGE);
+	var jiraUrl = response.data + '/browse/';
+    var styles = 'color:#00f; text-decoration: underline;';
+    var pattern = new RegExp("([a-zA-Z][a-zA-Z0-9_]+-[1-9][0-9]*)([^.]|\.[^0-9]|\.$|$)", 'ig');
+    var insert = ' <a style="' + styles + '" target="_blank" href="' + jiraUrl + '%s">%s</a> ';
+    var titleMatch = pattern.exec(title.text());
 
-    selectors.each(function(){
-		var self = $(this);
-		if(self.attr("tagName") == 'A') { return; }
-		
-		var match = pattern.exec(self.text());  
-		if(match[1]) {
-		    // @todo this doesn't work with multiple matches in the same text
-			self.html($(this).text().replace(
-				pattern,
-				' <a style="color:#00775a" target="_blank" href="' + jira + match[1] + '">' + match[1] + '</a>'
-			));
+    if (titleMatch && titleMatch[1]) {
+        title.html(title.text().replace(pattern, insert.replace(/%s/g, titleMatch[1])));
+    }
+
+    commits.each( function() {
+		var match = pattern.exec($(this).attr('title'));
+
+		if (match && match[1]) {
+            // remove ticket #
+            $(this).html($(this).text().replace(match[1], ''));
+            // replace it w a link
+            $(insert.replace(/%s/g, match[1]) + '&nbsp;').insertBefore(this);
 		}
+
 	});
 });
